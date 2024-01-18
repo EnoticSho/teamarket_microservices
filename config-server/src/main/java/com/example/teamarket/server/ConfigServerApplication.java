0bf8934fd4fffd206fd7adf1fbc79e174f65ba3c
EnoticSho <paradise666hell@mail.ru>
2024-01-18 17:40:59 +0300

    fix: docker

--- config-server/src/main/java/com/example/teamarket/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/teamarket/server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.teamarket.config;
+package com.example.teamarket.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
